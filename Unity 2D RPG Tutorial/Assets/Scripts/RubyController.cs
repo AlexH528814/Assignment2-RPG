b66b646be0bd20f3676a5a6b20016174b1d9fb95
AlexH528814 <528814@escg.ac.uk>
2022-10-05 21:54:41 +0100

    Continued the unity tutorial

--- Unity 2D RPG Tutorial/Assets/Scripts/RubyController.cs
+++ Unity 2D RPG Tutorial/Assets/Scripts/RubyController.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] public int maxHealth = 5;
+                     public int health { get { return currentHealth; }}
+    [SerializeField] int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Debug.Log(horizontal);
-        Debug.Log(vertical);
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 0.1f * horizontal * speed * Time.deltaTime;
         position.y = position.y + 0.1f * vertical * speed * Time.deltaTime;
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+
+   public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
